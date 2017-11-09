9eeae771e38ac63564fa0c9282970d706125da99
Yamihoshi <Yamihoshi@users.noreply.github.com>
2017-11-09 19:22:27 +0100

    Revert "Cors XML"
    This reverts commit 54e6e5de8fabe8df9de5ffa6eb53e8067a2e96c8.

--- src/api/filter/CORSFilter.java
+++ src/api/filter/CORSFilter.java
@@ -11,23 +11,20 @@ public class CORSFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {}
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-    	 HttpServletRequest request = (HttpServletRequest) servletRequest;
-         System.out.println("CORSFilter HTTP Request: " + request.getMethod());
-         System.out.println("CORSFilter HTTP Path Info: " + request.getPathInfo());
-         // Authorize (allow) all domains to consume the content
-         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
-         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
-  
-         HttpServletResponse resp = (HttpServletResponse) servletResponse;
-  
-         // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
-         if (request.getMethod().equals("OPTIONS")) {
-             resp.setStatus(HttpServletResponse.SC_ACCEPTED);
-             return;
-         }
-  
-         // pass the request along the filter chain
-         chain.doFilter(request, servletResponse);
+    HttpServletRequest request = (HttpServletRequest) servletRequest;
+    System.out.println("Request: " + request.getMethod());
+
+    HttpServletResponse resp = (HttpServletResponse) servletResponse;
+    resp.addHeader("Access-Control-Allow-Origin","*");
+    resp.addHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+    resp.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+
+    // Just ACCEPT and REPLY OK if OPTIONS
+    if ( request.getMethod().equals("OPTIONS") ) {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        return;
+    }
+    chain.doFilter(request, servletResponse);
 }
 
  @Override
