aa4fe5448885627196b4575e9c023c68072583d3
kevin <gesnouin.kevin@gmail.com>
2017-11-11 21:03:39 +0100

    recup info api sport nba

--- src/api/scheduler/BackgroundScheduler.java
+++ src/api/scheduler/BackgroundScheduler.java
@@ -14,8 +14,9 @@ public class BackgroundScheduler implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent event) {
-        //scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler = Executors.newSingleThreadScheduledExecutor();
         //scheduler.scheduleAtFixedRate(new DatabaseUpdater(), 0, 15, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(new DatabaseSportUpdater(), 0, 15, TimeUnit.MINUTES);
     }
 
     @Override
