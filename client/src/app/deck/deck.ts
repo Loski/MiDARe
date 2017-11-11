02cf934717a701da09f3da7865c7cd8c34e598a8
Maxime <Loski@users.noreply.github.com>
2017-11-11 19:29:29 +0100

    Implémentation interface

--- client/src/app/deck/deck.ts
+++ client/src/app/deck/deck.ts
@@ -4,19 +4,24 @@ import {Card} from './card/card'
 export namespace deckFields {
   export type idDeck = number;
   export type name = string;
-  export type cards = Card;
+  export type cards = Card[];
 }
 
 export interface DeckInterface {
   idDeck: deckFields.idDeck;
   name: deckFields.name;
-
+  cards: deckFields.cards;
 }
 
 
 export class Deck implements DeckInterface {
+  idDeck: number;
+  name: string;
+  cards: Card[];
 
-  constructor() {
-
+  constructor(idDeck: number, name: string, cards: Card[]) {
+    this.idDeck = idDeck;
+    this.name = name;
+    this.cards = cards;
   }
 }
